ddeb2f69cd055dad7816f6db3616d9ac7241b2e1
hfx <1262506231@qq.com>
2019-04-02 14:11:57 +0800

    init file.

---
+++ get_fasttext_model.py
@@ -0,0 +1,26 @@
+'''
+  @Author: DongGaocai 
+  @Date: 2018-12-29 15:13:54 
+  @Last Modified by: DongGaocai 
+  @Last Modified time: 2018-12-29 15:13:54 
+'''
+import warnings, os, pickle
+warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
+from gensim.models import FastText
+
+
+if not os.path.exists("AImed.model"):
+    # 将语料库转化为fasttext需要的格式
+    package = list()
+    with open("further corpus.txt","r",encoding="utf-8") as f:
+        content = f.readlines()
+        for sentence in content:
+            package.append(sentence.split())  # package相当于二维数组
+    model = FastText(package,size=20,window=3,min_count=1,iter=10)s
+    model.save("AImed.model")
+
+print("model saved")
+        
+    
+
+
---
+++ util/count_words.py
@@ -0,0 +1,22 @@
+'''
+  @Author: DongGaocai 
+  @Date: 2018-12-29 15:16:57 
+  @Last Modified by: DongGaocai 
+  @Last Modified time: 2018-12-29 15:16:57 
+'''
+import io
+import sys
+#改变标准输出的默认编码
+sys.stdout=io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
+
+# count the number of the words in the txt, including drop the same words
+L = []
+with open('further corpus.txt',encoding='utf-8') as f:
+    text = f.read()
+    L = L + text.split()
+print('一共有{}个词'.format(len(L)))
+del_same_list = list(set(L))
+print('去重后有{}个词'.format(len(del_same_list))) 
+
+# 一共有63462个词
+# 去重后有2573个词
